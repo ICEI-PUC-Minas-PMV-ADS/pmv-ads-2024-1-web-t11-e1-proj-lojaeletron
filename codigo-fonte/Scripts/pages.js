164bc9265b05daeea42c3da65a28683373732821
Nakazatoz <gabrielhna7@gmail.com>
2024-05-11 14:35:20 -0300

    Implementação de pesquisa
    Login/cadastro

--- codigo-fonte/Scripts/pages.js
+++ codigo-fonte/Scripts/pages.js
@@ -3,6 +3,52 @@ fetch('../Includes/header.html')
     .then(response => response.text())
     .then(data => {
     document.getElementById('header').innerHTML = data;
+}).then(() => {
+    const content = document.querySelector(".content");
+    const inputSearch = document.querySelector("input[type='search']");
+
+    let products = []; // Armazenará todos os produtos
+
+    // Carrega o arquivo JSON localmente
+    fetch("../Scripts/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+        // Armazena todos os produtos
+        products = data.Produtos;
+    })
+    .catch((error) => {
+        console.error('Erro ao carregar o arquivo JSON:', error);
+    });
+
+    // Search Button
+    inputSearch.addEventListener("input", () => {
+
+        const searchTerm = inputSearch.value.toLowerCase();
+        const filteredProducts = products.filter((product) => {
+            // Verifica se o nome do produto ou a marca do produto contém o termo de pesquisa
+            return product.nome.toLowerCase().includes(searchTerm) ||
+                   product.marca.toLowerCase().includes(searchTerm);
+        });
+    
+        // Exibe ou oculta os produtos com base no resultado da pesquisa
+        displayProducts(filteredProducts);
+    });
+
+    // List Product
+    function displayProducts(products) {
+        // Limpa o conteúdo atual
+        content.innerHTML = '';
+    
+        // Mostra os produtos apenas se a pesquisa não estiver vazia
+        if (products.length > 0 && inputSearch.value.trim() !== '') {
+            products.forEach((product) => {
+                const div = document.createElement("div");
+                // Exibe o nome e a marca do produto
+                div.textContent = `${product.nome} ${product.marca}`;
+                content.appendChild(div);
+            });
+        }
+    }
 });
 
 fetch('../Includes/carrousel.html')
@@ -40,5 +86,3 @@ fetch('../Includes/carrinho.html')
     .then(data => {
     document.getElementById('carrinho').innerHTML = data;
 });
-
-
