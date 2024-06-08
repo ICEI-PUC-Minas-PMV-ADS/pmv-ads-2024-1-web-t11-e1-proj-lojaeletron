c6abe49a3d36ad0a89a9f6c105d2f5a9d04aa8b5
KvN <mr.estrela@yahoo.com.br>
2024-06-08 11:17:40 -0300

    Implementacao da Newsletter
    Newsletter integrada em todas as paginas

---
+++ codigo-fonte/Scripts/script.js
@@ -0,0 +1,25 @@
+document.getElementById('newsletter-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Obter o email do usuário
+    var userEmail = document.getElementById('user-email').value;
+
+    // Mostrar mensagem de status
+    var statusMessage = document.getElementById('status-message');
+    statusMessage.textContent = "Enviando...";
+
+    // Configurar os parâmetros do template
+    var templateParams = {
+        user_email: userEmail
+    };
+
+    // Enviar o email usando EmailJS
+    emailjs.send('service_zhrrxaa', 'template_86ms9no', templateParams)
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+            statusMessage.textContent = "Inscrição realizada com sucesso!";
+        }, function(error) {
+            console.log('FAILED...', error);
+            statusMessage.textContent = "Ocorreu um erro ao tentar se inscrever. Tente novamente.";
+        });
+});
